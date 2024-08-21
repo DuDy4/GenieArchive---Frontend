f9e18daf7875eafb0b936c3c014c617bd2972aaa
Dan Shevel <dan.shevel@trywonder.ai>
2024-08-21 14:32:31 +0300

    Added handling

--- src/components/MeetingOverviewComponents/MeetingDetails.tsx
+++ src/components/MeetingOverviewComponents/MeetingDetails.tsx
@@ -8,24 +8,22 @@ import NewsSection from './NewsSection'; // Import the NewsSection component
 const MeetingDetails: React.FC<{ data: any }> = ({ data }) => {
   const { meeting, company, participants} = data;
   const guidelinesObject = data.meeting.guidelines;
-    const news = company.news; // Get the news data from the company object
+    const news = company ? company.news : undefined;
 
   return (
     <div className="meeting-details p-6">
       <h2 className="text-3xl font-bold mb-6">{meeting.subject}</h2>
 
       <div className="flex justify-between">
-        <div className="flex-1 mr-8">
+        {company ? <div className="flex-1 mr-8">
           <CompanyOverview overview={company.overview} />
           <CompanyDetails details={company} />
-        </div>
+        </div> : null}
 
         <div className="flex-1">
-          <Participants participants={participants} />
-          <MeetingGuidelines guidelinesObject={guidelinesObject} />
-
-          {/* Add the NewsSection here */}
-          <NewsSection news={news} />
+          {participants ? <Participants participants={participants} /> : null}
+          {guidelinesObject ? <MeetingGuidelines guidelinesObject={guidelinesObject} /> : null}
+          {news ? <NewsSection news={news} /> : null}
         </div>
       </div>
     </div>
