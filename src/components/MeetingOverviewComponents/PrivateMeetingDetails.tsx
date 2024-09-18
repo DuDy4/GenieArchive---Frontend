cb8bd5b5b86334a19eb66989610ba6d1d976f587
Dan Shevel <dan.shevel@trywonder.ai>
2024-09-18 13:36:39 +0300

    Better text in private meetings

--- src/components/MeetingOverviewComponents/PrivateMeetingDetails.tsx
+++ src/components/MeetingOverviewComponents/PrivateMeetingDetails.tsx
@@ -7,14 +7,13 @@ const PrivateMeetingDetails: React.FC<{ data: any }> = ({ data }) => {
     <div className="flex justify-center items-center">
       <div
         className="meeting-details bg-white rounded-full shadow-lg flex flex-col justify-center items-center"
-        style={{ width: '500px', height: '500px' }} // Increased circle size
+        style={{ width: '600px', height: '500px', padding: '25px'}} // Increased circle size
       >
         <h1 className="text-2xl font-bold text-gray-800 mb-4">Private Meeting</h1>
         <h2 className="text-lg font-semibold text-gray-500 text-center">
           We do not process your private meeting. <br />
-          This is a time for yourself.
+            Our Genie only shares the scoop on external meetings where the real action is.
         </h2>
-        {/* Genie Illustration */}
 
       </div>
     </div>
