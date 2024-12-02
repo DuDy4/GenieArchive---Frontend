cdb74d3179a9e62d50aad972cee55b71b8552732
Dan Shevel <dan.shevel@trywonder.ai>
2024-12-02 17:06:19 +0200

    ProfileCategoryDialog button now change color on hover

--- src/components/ProfilePageComponents/ProfileCategoryDialog.tsx
+++ src/components/ProfilePageComponents/ProfileCategoryDialog.tsx
@@ -12,6 +12,24 @@ interface Explanation {
   recommendations: string;
 }
 
+const darkenColor = (color: string, percent: number) => {
+  const num = parseInt(color.replace("#", ""), 16);
+  const amt = Math.round(2.55 * percent);
+  const R = (num >> 16) + amt;
+  const G = ((num >> 8) & 0x00ff) + amt;
+  const B = (num & 0x0000ff) + amt;
+  return (
+    "#" +
+    (
+      0x1000000 +
+      (R < 255 ? (R < 1 ? 0 : R) : 255) * 0x10000 +
+      (G < 255 ? (G < 1 ? 0 : G) : 255) * 0x100 +
+      (B < 255 ? (B < 1 ? 0 : B) : 255)
+    )
+      .toString(16)
+      .slice(1)
+  );
+};
 
 const titlize = (str: string) => {
     return str.charAt(0).toUpperCase() + str.slice(1);
@@ -28,14 +46,34 @@ const ProfileCategoryDialog: React.FC<ProfileCategoryProps> = ({ profileCategory
 
   return (
     <>
-      <div onClick={handleDialogOpen} className="py-[18px] pb-[20px] space-y-3 px-[12px] rounded-[1px] border border-[#dddddd]" style={{
-             display: 'flex',
-             flexDirection: 'column',
-             alignItems: 'center',
-             backgroundColor: profileCategory ? profileCategory.color : '#FFCC00',
-             cursor: 'pointer',
-           }}>
-          <p className="text-[24px]" style={{fontFamily: "Poppins"}}><strong>{titlize(profileCategory.category.replace('The ', ''))}</strong></p>
+      <div
+        onClick={handleDialogOpen}
+        className="py-[18px] pb-[20px] space-y-3 px-[12px] rounded-[1px] border border-[#dddddd]"
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          backgroundColor: profileCategory ? profileCategory.color : '#FFCC00',
+          cursor: 'pointer',
+          transition: 'background-color 0.3s ease',
+        }}
+        onMouseEnter={(e) => {
+          if (profileCategory?.color) {
+            (e.target as HTMLElement).style.backgroundColor = darkenColor(
+              profileCategory.color,
+              -10 // Darken by 10%
+            );
+          }
+        }}
+        onMouseLeave={(e) => {
+          if (profileCategory?.color) {
+            (e.target as HTMLElement).style.backgroundColor = profileCategory.color;
+          }
+        }}
+      >
+        <p className="text-[24px]" style={{ fontFamily: "Poppins" }}>
+          <strong>{titlize(profileCategory.category.replace('The ', ''))}</strong>
+        </p>
       </div>
 
       <Dialog open={isDialogOpen} onClose={handleDialogClose} maxWidth="md">
